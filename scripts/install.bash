ee8b1c7b2b4274dd4f00fbc6f70218be37090815
David Alejo Teissière <churr.te@gmail.com>
2021-03-12 15:58:30 +0100

    Update install.bash

--- scripts/install.bash
+++ scripts/install.bash
@@ -14,7 +14,7 @@ rosinstall . file:robotics_challenge/robotics_challenge.rosinstall
 
 ## Install yocs stuff
 P=ros-melodic-yocs-
-sudo aptitude install ${P}ar-marker-tracking ${P}ar-pair-approach ${P}ar-pair-tracking ${P}cmd-vel-mux ${P}controllers \
+sudo apt install ${P}ar-marker-tracking ${P}ar-pair-approach ${P}ar-pair-tracking ${P}cmd-vel-mux ${P}controllers \
 ${P}joyop ${P}keyop ${P}localization-manager ${P}math-toolkit ${P}msgs ${P}navi-toolkit ${P}navigator ${P}rapps ${P}safety-controller ${P}velocity-smoother ${P}virtual-sensor ${P}waypoint-provider ${P}waypoints-navi -y
 
 # Install kobuki stuff
