330ec7eef9047ad6d60da8a0ddd81e6a0e2f7674
david-alejo <churr.te@gmail.com>
2021-04-26 14:38:08 +0200

    Fixed initial positions

--- scripts/robotics_challenge_1.bash
+++ scripts/robotics_challenge_1.bash
@@ -1,3 +1,3 @@
 #! /bin/bash
 export TURTLEBOT3_MODEL=burger
-roslaunch robotics_challenge robotics_challenge.launch 
+roslaunch robotics_challenge robotics_challenge.launch init_x_mcl:=0 init_y_mcl:=0
--- scripts/robotics_challenge_2.bash
+++ scripts/robotics_challenge_2.bash
@@ -1,2 +1,2 @@
 #! /bin/bash
-roslaunch robotics_challenge robotics_challenge.launch scenario:=obstacle
+roslaunch robotics_challenge robotics_challenge.launch scenario:=obstacle init_x_mcl:=0 init_y_mcl:=0
